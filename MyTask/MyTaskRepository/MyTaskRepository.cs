dc54a1ed966a9bf886fb68b6fac8427617fa8981
asarker <bd.arup@gmail.com>
2017-05-16 20:40:53 +1000

    creating My task repository

---
+++ MyTask/MyTaskRepository/MyTaskRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyTaskRepository
+{
+    class MyTaskRepository
+    {
+        public interface IRepository<TEntity> where TEntity : class
+        {
+            TEntity Get(int id);
+            IEnumerable<TEntity> GetAll();
+            IEnumerable<TEntity> Find();
+
+            void Add(TEntity entity);
+            void AddRange(IEnumerable<TEntity> entites);
+
+            void Remove(TEntity entity);
+            void RemoveRange(IEnumerable<TEntity> entities);
+        }
+    }
+}
